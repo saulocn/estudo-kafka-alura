eec490467295b46a8713a5ebb808882808257d38
Saulo de Carvalho Neto <saulocn@gmail.com>
2019-12-30 11:21:08 -0300

    Múltiplos consumers de Kafka em Java

---
+++ projetos/ecommerce/src/main/java/br/com/alura/ecommerce/LogService.java
@@ -0,0 +1,46 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+    public static void main(String[] args) {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println(records.count() + "registros encontrados!");
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("-----------------------------------------");
+                    System.out.println("Log - " + record.topic());
+                    System.out.println(record.key());
+                    System.out.println(record.value());
+                    System.out.println(record.partition());
+                    System.out.println(record.offset());
+                    System.out.println(record.timestamp());
+                    System.out.println("-----------------------------------------");
+                }
+            }
+        }
+
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+
+        return properties;
+    }
+}
--- projetos/ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ projetos/ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -12,16 +13,22 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
         String value = "1234,4321,31231.223";
+        String email = "Thank you! We are processing your order!";
         ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value );
 
-        producer.send(record, (data, ex) -> {
-            if(ex!=null){
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("Sucess - topic:" + data.topic()+":::partition "+data.partition()+"/ offset "+data.offset()+"/ timestamp "+data.timestamp());
+            System.out.println("Sucess - topic:" + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
             return;
-        }).get();
+        };
+
+        ProducerRecord<String, String> emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email );
+        producer.send(record,  callback).get();
+        producer.send(emailRecord,callback).get();
+
     }
 
     private static Properties properties() {
