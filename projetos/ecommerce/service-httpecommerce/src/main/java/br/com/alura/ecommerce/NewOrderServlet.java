45f0df04dee8a65ee65ab924a3e5e50ba3a7b129
Saulo de Carvalho Neto <saulocn@gmail.com>
2019-12-30 23:59:36 -0300

    Refatorando o Servlet

--- projetos/ecommerce/service-httpecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
+++ projetos/ecommerce/service-httpecommerce/src/main/java/br/com/alura/ecommerce/NewOrderServlet.java
@@ -13,28 +13,34 @@ import java.util.concurrent.ExecutionException;
 import static java.util.UUID.randomUUID;
 
 public class NewOrderServlet extends HttpServlet {
+    KafkaDispatcher orderDispatcher = new KafkaDispatcher<Order>();
+    KafkaDispatcher emailDispatcher = new KafkaDispatcher<String>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try(KafkaDispatcher orderDispatcher = new KafkaDispatcher<Order>()) {
-            try(KafkaDispatcher emailDispatcher = new KafkaDispatcher<String>()) {
-                try {
-                    String email = req.getParameter("email");
-                    String orderId = randomUUID().toString();
-                    BigDecimal amount = new BigDecimal(req.getParameter("amount"));
-                    Order order = new Order(email, orderId, amount);
-                    String emailCode = "Thank you! We are processing your order!";
+        try {
+            String email = req.getParameter("email");
+            String orderId = randomUUID().toString();
+            BigDecimal amount = new BigDecimal(req.getParameter("amount"));
+            Order order = new Order(email, orderId, amount);
+            String emailCode = "Thank you! We are processing your order!";
 
-                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
-                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
-                    System.out.println("New order sent successfully!");
-                    resp.setStatus(HttpServletResponse.SC_CREATED);
-                    resp.getWriter().println("New order sent successfully!");
-                } catch (ExecutionException e) {
-                    throw new ServletException(e);
-                } catch (InterruptedException e) {
-                    throw new ServletException(e);
-                }
-            }
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
+            System.out.println("New order sent successfully!");
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            resp.getWriter().println("New order sent successfully!");
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
         }
     }
 }
